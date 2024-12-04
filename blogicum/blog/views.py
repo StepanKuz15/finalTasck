e0cbd72f38db0c4dcaa770143a74dc973ce285b7
Kuznetsov Stepan <stepakuz15@gmail.com>
2024-12-04 08:40:17 +0300

    Start

---
+++ blogicum/blog/views.py
@@ -0,0 +1,46 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.utils import timezone
+from django.db.models import Q
+from blog.models import Post, Category
+
+def index(request):
+    template_name = 'blog/index.html'
+    postsbd = Post.objects.filter(
+        pub_date__lte=now,
+        is_published=True,
+        category__is_published=True
+    ).order_by('-pub_date')[:5]
+    context = {'postsbd': postsbd}
+    return render(request, template_name, context)
+
+def post_detail(request, id):
+    template_name = 'blog/detail.html'
+    post = get_object_or_404(Post, id=id)
+    if post.pub_date > timezone.now():
+        raise Http404("Публикация еще не доступна.")
+    elif not post.is_published:
+        raise Http404("Публикация не найдена или не опубликована.")
+    elif not post.category.is_published:
+        raise Http404("Категория публикации не найдена или не опубликована.")
+    context = {'post': post}
+    return render(request, template_name, context)
+
+
+def category_posts(request, category_slug):
+    template_name = 'blog/category.html'
+    category = get_object_or_404(Category, slug=category_slug)
+    if not category.is_published:
+        raise Http404("Категория не найдена или не опубликована.")
+    postsbd = Post.objects.filter(
+        category=category,
+        pub_date__lte=now,
+        is_published=True,
+        )
+    context = {
+        'postsbd': postsbd,
+        'category': category,  
+    }
+    return render(request, template_name, context)
+
+now = timezone.now()
